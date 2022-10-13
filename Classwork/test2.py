c97d7ac2f6c5d233f958e6452516ac627162a448
RakeshKharva <rakeshkharva.tops@gmail.com>
2022-10-13 17:38:28 +0530

    g

---
+++ Classwork/test2.py
@@ -0,0 +1,15 @@
+"""
+a = input("Enter First Name : ")
+b = input("Enter Last Name : ")
+print(a," ",b)
+print(type(a))
+
+"""
+
+a = int(input("Enter A : "))
+b = int(input("Enter B : "))
+print(type(a))
+print("Addition : ",a+b)
+print("Subtraction : ",a-b)
+print("Multiplication : ",a*b)
+print("Division : ",a/b)
